e2ed0bafd04af1bbcb52a45130317e38a3e789d4
Raiyan Noory Rady <raiyannooryrady@gmail.com>
2023-06-06 09:30:33 +0600

    updated code

--- footer.php
+++ footer.php
@@ -46,7 +46,7 @@
 
                 </ul>
                 <p> <?php _e('Copyright ©','behealthy'); echo date('Y'); ?> <a href="<?php echo site_url(); ?>" class="text-decoration-none link-danger"><?php bloginfo("name"); ?></a> <?php _e('|| All
-                    rights reserved','behealthy');?><br> <a href="http://liilab.com" class="text-decoration-none link-danger" target="_blank"><?php _e('BeHealthy-Theme ','behealthy'); ?></a><?php _e('by','behealthy'); ?><a href="http://liilab.com" class="text-decoration-none link-danger" target="_blank"><?php _e(' LIILab','behealthy'); ?></a>
+                    rights reserved','behealthy');?><br> <a href="#" class="text-decoration-none link-danger" target="_blank"><?php _e('BeHealthy-Theme ','behealthy'); ?></a><?php _e('by','behealthy'); ?><a href="#" class="text-decoration-none link-danger" target="_blank"><?php _e(' Raiyan Noory Rady','behealthy'); ?></a>
                 </p>
 
             </div>
